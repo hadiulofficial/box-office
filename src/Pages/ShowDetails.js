8644912081434116b4a610b3e69f9b498641abd3
hadiul-islam <hadiulislamofficial@gmail.com>
2022-02-02 21:05:48 +0600

    add useReducer on showDetails page

--- src/Pages/ShowDetails.js
+++ src/Pages/ShowDetails.js
@@ -1,22 +1,85 @@
-import React, { useState, useEffect } from 'react';
+import React, { useReducer, useEffect } from 'react';
+import loadingImg from '../assets/images/loading.gif'
 import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
+const reducer = (prevState, action) => {
+    switch(action.type){
+        case 'FETCH_SUCCESS': {
+            return {
+                showDetails: action.showDetails,
+                isLoading: false,
+                error: null
+            }
+        }
+
+        case 'FETCH_FAILED': {
+            return {
+                ...prevState,
+                isLoading: false,
+                error: action.error
+            }
+        }
+
+        default:
+            return prevState;
+    }
+}
+
+const initialState = {
+    showDetails: null,
+    isLoading: true,
+    error: null
+}
+
 const ShowDetails = () => {
-    const [showDetails, setShowDetails] = useState(null)
     const { id } = useParams()
-    
+
+    const [{showDetails, isLoading, error}, dispatch] = useReducer(reducer, initialState)
+
     useEffect(() => {
+        let isMouted = true;
+
         apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
-        .then(data => {
-            setShowDetails(data)
+            .then(data => {
+                if (isMouted) {
+                    dispatch({
+                        type: 'FETCH_SUCCESS',
+                        showDetails: data
+                    })
+                }
+            })
+            .catch(err => {
+                dispatch({
+                    type: 'FETCH_FAILED',
+                    error: err.message
+                })
+            })
+
+        return (() => {
+            isMouted = false;
         })
+
     }, [id]);
+
+    console.log('data: ',showDetails)
+
+    if (isLoading) {
+        return (
+            <div className="loading">
+                <img src={loadingImg} alt="loading.." />
+            </div>
+        )
+    }
+
+    if (error) {
+        return <div>{error}</div>
+    }
 
     return (
         <div>
             <h1>Details</h1>
-            
+
         </div>
     );
 };
